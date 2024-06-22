3e1c1ec1640371d79fe2f755b5feddf5f118a67f
LuizTheBlindDev <luizariano2@gmail.com>
2024-06-21 23:31:17 -0300

    refacturing code

--- Exericio8/Origem.cpp
+++ Exericio8/Origem.cpp
@@ -10,6 +10,9 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -27,6 +30,10 @@ using namespace std;
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include "Shader.h"
+
+#include "Mesh.h"
+
 // Protótipo da função de callback de teclado
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
@@ -34,97 +41,8 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 int setupShader();
 int setupGeometry();
 int loadTexture(string path);
-
-class triangulo {
-public:
-	GLfloat vertice1[6];
-	GLfloat vertice2[6];
-	GLfloat vertice3[6];
-	GLfloat vertices[18];
-	GLfloat position[3];
-	GLfloat color[3];
-	GLfloat size;
-
-	triangulo(GLfloat position[3], GLfloat size, GLfloat color[3]) {
-		this->position[0] = position[0];
-		this->position[1] = position[1];
-		this->position[2] = position[2];
-
-		this->size = size;
-
-		this->color[0] = color[0];
-		this->color[1] = color[1];
-		this->color[2] = color[2];
-	};
-
-			void verticeUpdate() {
-
-				vertice1[0] = position[0] + -size / 2;
-				vertice1[1] = position[1] + -size / 2;
-				vertice1[2] = position[2];
-				vertice1[3] = color[0];
-				vertice1[4] = color[1];
-				vertice1[5] = color[2];
-
-				vertice2[0] = position[0] + -size / 2;
-				vertice2[1] = position[1] + size / 2;
-				vertice2[2] = position[2];
-				vertice2[3] = color[0];
-				vertice2[4] = color[1];
-				vertice2[5] = color[2];
-
-				vertice3[0] = position[0] + size / 2;
-				vertice3[1] = position[1] + -size / 2;
-				vertice3[2] = position[2];
-				vertice3[3] = color[0];
-				vertice3[4] = color[1];
-				vertice3[5] = color[2];
-
-				vertices[0] = vertice1[1];
-				vertices[1] = vertice1[2];
-				vertices[2] = vertice1[3];
-				vertices[3] = vertice1[4];
-				vertices[4] = vertice1[5];
-				vertices[5] = vertice1[6];
-
-				vertices[6] = vertice2[0];
-				vertices[7] = vertice2[1];
-				vertices[8] = vertice2[2];
-				vertices[9] = vertice2[3];
-				vertices[10] = vertice2[4];
-				vertices[11] = vertice2[5];
-
-				vertices[12] = vertice3[0];
-				vertices[13] = vertice3[1];
-				vertices[14] = vertice3[2];
-				vertices[15] = vertice3[3];
-				vertices[16] = vertice3[4];
-				vertices[17] = vertice3[5];
-			};
-
-	void updateTriangulo() {
-		GLfloat vertices1[] = {
-		-0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
-			-0.5, -0.5, 0.5, 1.0, 0.0, 0.0,
-			0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
-		};
-
-
-		for (int i = 0; i < 18; i++) {
-			vertices[i] = vertices1[i];
-		};
-	};
-
-	void setPosition(GLfloat x, GLfloat y, GLfloat z) {
-		position[0] = x;
-		position[1] = y;
-		position[2] = z;
-
-		updateTriangulo();
-	};
-
-
-};
+glm::mat4 doAction(string action, glm::mat4 model);
+int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color);
 
 // Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 1000, HEIGHT = 1000;
@@ -155,26 +73,13 @@ const GLchar* fragmentShaderSource = "#version 450\n"
 "color = texture(tex_buffer, texCoord);\n"
 "}\n\0";
 
-bool rotateX=false, rotateY=false, rotateZ=false, pressW=false, pressA = false, pressS = false, pressD = false, pressQ = false, pressE = false;
+string keyAction = "";
 
 // Função MAIN
 int main()
 {
 	// Inicialização da GLFW
 	glfwInit();
-
-	//Muita atenção aqui: alguns ambientes não aceitam essas configurações
-	//Você deve adaptar para a versão do OpenGL suportada por sua placa
-	//Sugestão: comente essas linhas de código para desobrir a versão e
-	//depois atualize (por exemplo: 4.5 com 4 e 5)
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	//Essencial para computadores da Apple
-//#ifdef __APPLE__
-//	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-//#endif
 
 	// Criação da janela GLFW
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola 3D -- Luiz Silva!", nullptr, nullptr);
@@ -184,10 +89,8 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 
 	// GLAD: carrega todos os ponteiros d funções da OpenGL
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
 		std::cout << "Failed to initialize GLAD" << std::endl;
-
 	}
 
 	// Obtendo as informações de versão
@@ -201,20 +104,22 @@ int main()
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
-
 	// Compilando e buildando o programa de shader
-	GLuint shaderID = setupShader();
+	// GLuint shaderID = setupShader();
+	 
+	Shader shader("vertex.vs", "fragment.fs");
 
 	GLuint texID = loadTexture("../textures/Logosheep.png");
 
-	// Gerando um buffer simples, com a geometria de um triângulo
-	GLuint VAO = setupGeometry();
+	int nVerts;
+	GLuint VAO = loadSimpleOBJ("../../3D_models/Suzanne/suzanneTriLowPoly.obj", nVerts);
+
+
+	Mesh suzanne;
+	suzanne.initialize(VAO, nVerts, &shader, glm::vec3(-2.75, 0.0, 0.0));
+
+
 	glUseProgram(shaderID);
-	glm::mat4 model = glm::mat4(1); //matriz identidade;
-	GLint modelLoc = glGetUniformLocation(shaderID, "model");
-	//
-	//model = glm::rotate(model, /*(GLfloat)glfwGetTime()*/glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	glUniformMatrix4fv(modelLoc, 1, FALSE, glm::value_ptr(model));
 
 	glEnable(GL_DEPTH_TEST);
 
@@ -232,55 +137,11 @@ int main()
 		glLineWidth(10);
 		glPointSize(20);
 
-		float angle = (GLfloat)glfwGetTime();
-		float scaleAmount = static_cast<float>(glfwGetTime());
-
 		model = glm::mat4(1); 
-		if (rotateX)
-		{
-			model = glm::rotate(model, angle, glm::vec3(1.0f, 0.0f, 0.0f));
-			
-		}
-		else if (rotateY)
-		{
-			model = glm::rotate(model, angle, glm::vec3(0.0f, 1.0f, 0.0f));
-
-		}
-		else if (rotateZ)
-		{
-			model = glm::rotate(model, angle, glm::vec3(0.0f, 0.0f, 1.0f));
-
-		}
-		
-		if(pressW) {
-			model = glm::translate(model, glm::vec3(0.0f, scaleAmount, 0.0f));
-		} 
-
-		else if (pressS) {
-			model = glm::translate(model, glm::vec3(0.0f, -scaleAmount, 0.0f));
-		}
-
-		else if (pressA) {
-			model = glm::translate(model, glm::vec3(-scaleAmount, 0.0f, 0.0f));
-		}
-
-		else if (pressD) {
-			model = glm::translate(model, glm::vec3(scaleAmount, 0.0f, 0.0f));
-		}
-
-		else if (pressQ) {
-			model = glm::scale(model, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
-		}
-
-		else if (pressE) {
-			model = glm::scale(model, glm::vec3(1 / scaleAmount, 1 / scaleAmount, 1 / scaleAmount));
-		}
-		
+		model = doAction(keyAction, model);
 
 		glUniformMatrix4fv(modelLoc, 1, FALSE, glm::value_ptr(model));
-		// Chamada de desenho - drawcall
-		// Poligono Preenchido - GL_TRIANGLES
-		
+
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texID);
 
@@ -289,20 +150,11 @@ int main()
 
 		glBindVertexArray(0); //unbind - desconecta
 		glBindTexture(GL_TEXTURE_2D, 0); //unbind da textura
-
-		// Chamada de desenho - drawcall
-		// CONTORNO - GL_LINE_LOOP
-		
-		model = glm::mat4(1);
-		model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
-		model = glm::translate(model, glm::vec3(-2.5f, 0.0f, 0.0f));
-		glUniformMatrix4fv(modelLoc, 1, FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 48);
+	
 
 		glDrawArrays(GL_POINTS, 0, 48);
 		glBindVertexArray(0);
 
-		// Troca os buffers da tela
 		glfwSwapBuffers(window);
 	}
 	// Pede pra OpenGL desalocar os buffers
@@ -312,9 +164,52 @@ int main()
 	return 0;
 }
 
-// Função de callback de teclado - só pode ter uma instância (deve ser estática se
-// estiver dentro de uma classe) - É chamada sempre que uma tecla for pressionada
-// ou solta via GLFW
+
+glm::mat4 doAction(string keyAction, glm::mat4 model) {
+
+	float angle = (GLfloat)glfwGetTime();
+	float scaleAmount = static_cast<float>(glfwGetTime());
+
+	if (keyAction == "rotateX"){
+		model = glm::rotate(model, angle, glm::vec3(1.0f, 0.0f, 0.0f));
+	}
+
+	else if (keyAction == "rotateY"){
+		model = glm::rotate(model, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+	}
+
+	else if (keyAction == "rotateZ"){
+		model = glm::rotate(model, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+	}
+
+	else if (keyAction == "pressW") {
+		model = glm::translate(model, glm::vec3(0.0f, scaleAmount, 0.0f));
+	}
+
+	else if (keyAction == "pressS") {
+		model = glm::translate(model, glm::vec3(0.0f, -scaleAmount, 0.0f));
+	}
+
+	else if (keyAction == "pressA") {
+		model = glm::translate(model, glm::vec3(-scaleAmount, 0.0f, 0.0f));
+	}
+
+	else if (keyAction == "pressD") {
+		model = glm::translate(model, glm::vec3(scaleAmount, 0.0f, 0.0f));
+	}
+
+	else if (keyAction == "pressQ") {
+		model = glm::scale(model, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+	}
+
+	else if (keyAction == "pressE") {
+		model = glm::scale(model, glm::vec3(1 / scaleAmount, 1 / scaleAmount, 1 / scaleAmount));
+	}
+
+	return model;
+}
+
+// Função de callback de teclado 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
@@ -322,127 +217,86 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 	if (key == GLFW_KEY_X && action == GLFW_PRESS)
 	{
-		if (rotateX == true) {
-			rotateX = false;
+		if (keyAction == "rotateX") {
+			keyAction = " ";
 			glfwSetTime(0);
-		} else rotateX = true;
-		rotateY = false;
-		rotateZ = false;
+		}
+		else keyAction = "rotateX";
 	}
 
 	if (key == GLFW_KEY_Y && action == GLFW_PRESS)
 	{
-		rotateX = false;
-		if (rotateY == true) {
-			rotateY = false;
+		if (keyAction == "rotateY") {
+			keyAction = " ";
 			glfwSetTime(0);
 		}
-		else rotateY = true;
-		rotateZ = false;
+		else keyAction = "rotateY";
 	}
 
 	if (key == GLFW_KEY_Z && action == GLFW_PRESS)
 	{
-		rotateX = false;
-		rotateY = false;
-		if (rotateZ == true) {
-			rotateZ = false;
+		if (keyAction == "rotateZ") {
+			keyAction = " ";
 			glfwSetTime(0);
 		}
-		else rotateZ = true;
+		else keyAction = "rotateZ";
 	}
 	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
-		if (rotateX == true || rotateY == true || rotateZ == true) {
-			rotateX = false;
-			rotateY = false;
-			rotateZ = false;
-		}
-
-		pressW = true;
+		 keyAction = "pressW";
 	}
 	if (key == GLFW_KEY_W && action == GLFW_RELEASE) {
-		pressW = false; 
+		keyAction = " ";
 		glfwSetTime(0);
 	}
 
 	if (key == GLFW_KEY_S && action == GLFW_PRESS) {
-		if (rotateX == true || rotateY == true || rotateZ == true) {
-			rotateX = false;
-			rotateY = false;
-			rotateZ = false;
-		}
-
-		pressS = true;
+		keyAction = "pressS";
 	}
+
 	if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
-		pressS = false;
+		keyAction = " ";
 		glfwSetTime(0);
 	}
 
 	if (key == GLFW_KEY_A && action == GLFW_PRESS) {
-		if (rotateX == true || rotateY == true || rotateZ == true) {
-			rotateX = false;
-			rotateY = false;
-			rotateZ = false;
-		}
-
-		pressA = true;
+		keyAction = "pressA";
 	}
+
 	if (key == GLFW_KEY_A && action == GLFW_RELEASE) {
-		pressA = false;
+		keyAction = " ";
 		glfwSetTime(0);
 	}
 
 	if (key == GLFW_KEY_D && action == GLFW_PRESS) {
-		if (rotateX == true || rotateY == true || rotateZ == true) {
-			rotateX = false;
-			rotateY = false;
-			rotateZ = false;
-		}
-
-		pressD = true;
+		keyAction = "pressD";
 	}
+
 	if (key == GLFW_KEY_D && action == GLFW_RELEASE) {
-		pressD = false;
+		keyAction = " ";
 		glfwSetTime(0);
 	}
 
 	if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
-		if (rotateX == true || rotateY == true || rotateZ == true) {
-			rotateX = false;
-			rotateY = false;
-			rotateZ = false;
-		}
 		glfwSetTime(1);
-		pressQ = true;
+		keyAction = "pressQ";
 	}
 
 	if (key == GLFW_KEY_Q && action == GLFW_RELEASE) {
-		pressQ = false;
+		keyAction = " ";
 		glfwSetTime(1);
 	}
 
 	if (key == GLFW_KEY_E && action == GLFW_PRESS) {
-		if (rotateX == true || rotateY == true || rotateZ == true) {
-			rotateX = false;
-			rotateY = false;
-			rotateZ = false;
-		}
 		glfwSetTime(1);
-		pressE = true;
-	}
-	if (key == GLFW_KEY_E && action == GLFW_RELEASE) {
-		pressE = false;
-		glfwSetTime(1);
+		keyAction = "pressE";
 	}
 
+	if (key == GLFW_KEY_E && action == GLFW_RELEASE) {
+		keyAction = " ";
+		glfwSetTime(1);
+	}
 }
 
-//Esta função está basntante hardcoded - objetivo é compilar e "buildar" um programa de
-// shader simples e único neste exemplo de código
-// O código fonte do vertex e fragment shader está nos arrays vertexShaderSource e
-// fragmentShader source no iniçio deste arquivo
-// A função retorna o identificador do programa de shader
 int setupShader()
 {
 	// Vertex shader
@@ -486,19 +340,8 @@ int setupShader()
 	return shaderProgram;
 }
 
-// Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a 
-// geometria de um triângulo
-// Apenas atributo coordenada nos vértices
-// 1 VBO com as coordenadas, VAO com apenas 1 ponteiro para atributo
-// A função retorna o identificador do VAO
-int setupGeometry()
-{
-	// Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
-	// sequencial, já visando mandar para o VBO (Vertex Buffer Objects)
-	// Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
-	// Pode ser arazenado em um VBO único ou em VBOs separados
-
-
+//Geometria de um cubo com textura
+int setupGeometry(){
 	GLfloat vertices[] = {
 
 		//Base da pirâmide: 2 triângulos
@@ -587,11 +430,7 @@ int setupGeometry()
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
-	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
-	// atualmente vinculado - para que depois possamos desvincular com segurança
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
 	glBindVertexArray(0);
 
 	return VAO;
@@ -640,4 +479,159 @@ int loadTexture(string path)
 	return texID;
 }
 
+int loadSimpleOBJ(string filepath, int& nVerts, glm::vec3 color)
+{
+	vector <glm::vec3> vertices;
+	vector <GLuint> indices;
+	vector <glm::vec2> texCoords;
+	vector <glm::vec3> normals;
+	vector <GLfloat> vbuffer;
 
+	ifstream inputFile;
+	inputFile.open(filepath.c_str());
+	if (inputFile.is_open())
+	{
+		char line[100];
+		string sline;
+
+
+
+		while (!inputFile.eof())
+		{
+			inputFile.getline(line, 100);
+			sline = line;
+
+			string word;
+
+			istringstream ssline(line);
+			ssline >> word;
+
+			//cout << word << " ";
+			if (word == "v")
+			{
+				glm::vec3 v;
+
+				ssline >> v.x >> v.y >> v.z;
+
+				vertices.push_back(v);
+			}
+			if (word == "vt")
+			{
+				glm::vec2 vt;
+
+				ssline >> vt.s >> vt.t;
+
+				texCoords.push_back(vt);
+			}
+			if (word == "vn")
+			{
+				glm::vec3 vn;
+
+				ssline >> vn.x >> vn.y >> vn.z;
+
+				normals.push_back(vn);
+			}
+			if (word == "f")
+			{
+				string tokens[3];
+
+				ssline >> tokens[0] >> tokens[1] >> tokens[2];
+
+				for (int i = 0; i < 3; i++)
+				{
+					//Recuperando os indices de v
+					int pos = tokens[i].find("/");
+					string token = tokens[i].substr(0, pos);
+					int index = atoi(token.c_str()) - 1;
+					indices.push_back(index);
+
+					vbuffer.push_back(vertices[index].x);
+					vbuffer.push_back(vertices[index].y);
+					vbuffer.push_back(vertices[index].z);
+					vbuffer.push_back(color.r);
+					vbuffer.push_back(color.g);
+					vbuffer.push_back(color.b);
+
+					//Recuperando os indices de vts
+					tokens[i] = tokens[i].substr(pos + 1);
+					pos = tokens[i].find("/");
+					token = tokens[i].substr(0, pos);
+					index = atoi(token.c_str()) - 1;
+
+					vbuffer.push_back(texCoords[index].s);
+					vbuffer.push_back(texCoords[index].t);
+
+					//Recuperando os indices de vns
+					tokens[i] = tokens[i].substr(pos + 1);
+					index = atoi(tokens[i].c_str()) - 1;
+
+					vbuffer.push_back(normals[index].x);
+					vbuffer.push_back(normals[index].y);
+					vbuffer.push_back(normals[index].z);
+				}
+			}
+
+		}
+
+	}
+	else
+	{
+		cout << "Problema ao encontrar o arquivo " << filepath << endl;
+	}
+	inputFile.close();
+
+	GLuint VBO, VAO;
+
+	nVerts = vbuffer.size() / 11; //Provisório
+
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, vbuffer.size() * sizeof(GLfloat), vbuffer.data(), GL_STATIC_DRAW);
+
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se está normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+
+	//Atributo posição (x, y, z)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Atributo cor (r, g, b)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	//Atributo coordenada de textura (s, t)
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	//Atributo normal do vértice (x, y, z)
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 11 * sizeof(GLfloat), (GLvoid*)(8 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(3);
+
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+
+	return VAO;
+
+}
